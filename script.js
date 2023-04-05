f03f367b6c3d4d17f3635ea722d56045064b2a35
bally4h <banfibalazs81@gmail.com>
2023-04-05 19:49:54 +0200

    push buttons show digits onscreen

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 let first = "";
 let second = "";
 let operator = "";
-let result = "";
-let partial = "";
+let resultNum = "";
+let partialNum = "";
 
+const partialScreen = document.querySelector('#partial')
+const resultScreen = document.querySelector('#result')
 
-const buttons = document.querySelectorAll('.buttons');
+const buttons = document.querySelectorAll('.digits');
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         console.log(button.id);
+        first = first.concat(button.id);
+        resultScreen.textContent = first;
     })
 });
