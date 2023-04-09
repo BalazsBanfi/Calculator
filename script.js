23ec925c13f46948ec89b986654ec6a1a342a4e3
bally4h <banfibalazs81@gmail.com>
2023-04-09 13:52:31 +0200

    show screens numbers, plus working

--- script.js
+++ script.js
@@ -1,17 +1,63 @@
 let first = "";
 let second = "";
+let lastPushedDigit = false;
 let operator = "";
 let resultNum = "";
 let partialNum = "";
+const partialScreen = document.querySelector('#partial');
+const resultScreen = document.querySelector('#result');
 
-const partialScreen = document.querySelector('#partial')
-const resultScreen = document.querySelector('#result')
+function reset() {
+    first = "";
+    second = "";
+    lastPushedDigit = false;
+    operator = "";
+    resultNum = "";
+    partialNum = "";
+    partialScreen.textContent = "";
+    resultScreen.textContent = "0";
+}
+
+reset();
+
+
 
 const buttons = document.querySelectorAll('.digits');
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        console.log(button.id);
-        first = first.concat(button.id);
-        resultScreen.textContent = first;
+        second = second.concat(button.id);
+        resultScreen.textContent = second;
     })
+});
+
+const ac = document.querySelector('#ac');
+ac.addEventListener('click', () => {
+    reset();
+});
+
+const c = document.querySelector('#c');
+c.addEventListener('click', () => {
+    resultScreen.textContent = "0";
+    second = "";
+});
+
+
+
+
+const plus = document.querySelector('#plus')
+plus.addEventListener('click', () => {
+    operator = "plus";
+    if (!first) {
+        first = parseInt(second);
+        partialScreen.textContent = `${first}+`;
+        second = ""; 
+    }
+    else {
+        partialScreen.textContent = `${first}+${second}`;
+        resultNum = first + parseInt(second);
+        first = resultNum;
+        second = "";
+    }
+    console.log(resultNum);
+    resultScreen.textContent = resultNum;
 });
